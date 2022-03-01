378ea6bb84c7f2892a2d065988247e74832e8d38
Jerome Albus <jerome.albus@rjglobus.local>
2022-03-01 17:39:19 +0800

    (Login screen by Albus)

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -10,11 +10,52 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
     <View style = {{ 
       flex: 1,
       backgroundColor: '#011627',
+      alignItems: 'center',
     }}>
-      {/* Username and Password fied */}
+      
+      <Text style={{
+        textAlign: 'center', color: '#F6F7F8', fontSize: 50, marginTop: '25%'
+      }}> 
+      Log In
+      </Text>
+      
+      {/* Username, Password, Buttons Container*/}
       <View style={{
-         
+         display: "flex",
+         alignItems: 'center',
+         height: '50%',
+         width: '90%',
+         backgroundColor: '#011627',
+         marginTop: '25%',
       }}>
+
+        <Text style={{
+          backgroundColor: '#F6F7F8', height: '15%', width: '80%',  borderRadius: 20, marginBottom: '10%', fontSize: 21, textAlign: "center", paddingTop: 10, color: '#4E4E4E'
+        }}>
+          Username
+        </Text>
+        <Text style={{
+          backgroundColor: '#F6F7F8', height: '15%', width: '80%',  borderRadius: 20, marginBottom: '10%', fontSize: 21, textAlign: "center", paddingTop: 10, color: '#4E4E4E'
+        }}>
+          Password
+        </Text>
+
+        <Text style={{
+          backgroundColor: '#2EC4B6', height: '15%', width: '80%',  borderRadius: 10, marginBottom: '5%', fontSize: 21, textAlign: "center", paddingTop: 10
+        }}>
+          Log In
+        </Text>
+
+        <Text style={{
+          textAlign: 'center', color: "#F6F7F8", fontSize: 23, fontWeight: "200",
+        }}>
+          or
+        </Text>
+        <Text style={{
+          backgroundColor: '#20A4F3', height: '15%', width: '80%',  borderRadius: 10, marginTop: '5%', marginBottom: '10%', fontSize: 21, textAlign: "center", paddingTop: 10
+        }}>
+          Register
+        </Text>
 
       </View>
     </View>
